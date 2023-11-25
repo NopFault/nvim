c051025a1f71d59c41b5d0db13938447379a08b7
nopfault <nopfault@gmail.com>
2023-11-25 19:24:32 +0200

    Add markdown to pdf converter
    Add possibility to convert markdown to pdf

--- init.lua
+++ init.lua
@@ -57,6 +57,8 @@ require("packer").startup(function(use)
         use 'feline-nvim/feline.nvim',
         use 'nvim-tree/nvim-web-devicons',
         use 'airblade/vim-gitgutter',
+        use "lukas-reineke/indent-blankline.nvim",
+        use 'arminveres/md-pdf.nvim',
     }
 end)
 
@@ -95,6 +97,11 @@ vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts)
 vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts)
 vim.keymap.set('n', "<C-f>", function() code_format() end, opts)
 
+-- MD to PDF 
+vim.keymap.set("n", "<leader>zz", function()
+    require('md-pdf').convert_md_to_pdf()
+end)
+
 
 -- BASIC CONFIG
 --
@@ -123,7 +130,7 @@ o.foldmethod = 'expr'
 o.foldexpr = 'nvim_treesitter#foldexpr()'
 
 
-vim.cmd[[autocmd BufWritePost *.swift :silent exec "!swiftformat %"]]
+vim.cmd [[autocmd BufWritePost *.swift :silent exec "!swiftformat %"]]
 
 --
 -- BUFFERS
@@ -145,6 +152,32 @@ require("tokyonight").setup({
 })
 vim.cmd [[colorscheme tokyonight]]
 
+-- INDENT VISUALISATION
+--
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup { indent = { highlight = highlight } }
 
 -- TREESITTER
 --
@@ -168,6 +201,7 @@ lsp.ensure_installed({
     "rust_analyzer",
     "lua_ls",
     "eslint",
+    "zls",
 })
 
 require("mason").setup()
@@ -237,227 +271,227 @@ require("toggleterm").setup()
 require('gitsigns').setup()
 
 local one_monokai = {
-	fg = "#abb2bf",
-	bg = "#1e2024",
-	green = "#98c379",
-	yellow = "#e5c07b",
-	purple = "#c678dd",
-	orange = "#d19a66",
-	peanut = "#f6d5a4",
-	red = "#e06c75",
-	aqua = "#61afef",
-	darkblue = "#282c34",
-	dark_red = "#f75f5f",
+    fg = "#abb2bf",
+    bg = "#1e2024",
+    green = "#98c379",
+    yellow = "#e5c07b",
+    purple = "#c678dd",
+    orange = "#d19a66",
+    peanut = "#f6d5a4",
+    red = "#e06c75",
+    aqua = "#61afef",
+    darkblue = "#282c34",
+    dark_red = "#f75f5f",
 }
 
 local vi_mode_colors = {
-	NORMAL = "green",
-	OP = "green",
-	INSERT = "yellow",
-	VISUAL = "purple",
-	LINES = "orange",
-	BLOCK = "dark_red",
-	REPLACE = "red",
-	COMMAND = "aqua",
+    NORMAL = "green",
+    OP = "green",
+    INSERT = "yellow",
+    VISUAL = "purple",
+    LINES = "orange",
+    BLOCK = "dark_red",
+    REPLACE = "red",
+    COMMAND = "aqua",
 }
 
 local c = {
-	vim_mode = {
-		provider = {
-			name = "vi_mode",
-			opts = {
-				show_mode_name = true,
-				-- padding = "center", -- Uncomment for extra padding.
-			},
-		},
-		hl = function()
-			return {
-				fg = require("feline.providers.vi_mode").get_mode_color(),
-				bg = "darkblue",
-				style = "bold",
-				name = "NeovimModeHLColor",
-			}
-		end,
-		left_sep = "block",
-		right_sep = "block",
-	},
-	gitBranch = {
-		provider = "git_branch",
-		hl = {
-			fg = "peanut",
-			bg = "darkblue",
-			style = "bold",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	gitDiffAdded = {
-		provider = "git_diff_added",
-		hl = {
-			fg = "green",
-			bg = "darkblue",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	gitDiffRemoved = {
-		provider = "git_diff_removed",
-		hl = {
-			fg = "red",
-			bg = "darkblue",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	gitDiffChanged = {
-		provider = "git_diff_changed",
-		hl = {
-			fg = "fg",
-			bg = "darkblue",
-		},
-		left_sep = "block",
-		right_sep = "right_filled",
-	},
-	separator = {
-		provider = "",
-	},
-	fileinfo = {
-		provider = {
-			name = "file_info",
-			opts = {
-				type = "relative-short",
-			},
-		},
-		hl = {
-			style = "bold",
-		},
-		left_sep = " ",
-		right_sep = " ",
-	},
-	diagnostic_errors = {
-		provider = "diagnostic_errors",
-		hl = {
-			fg = "red",
-		},
-	},
-	diagnostic_warnings = {
-		provider = "diagnostic_warnings",
-		hl = {
-			fg = "yellow",
-		},
-	},
-	diagnostic_hints = {
-		provider = "diagnostic_hints",
-		hl = {
-			fg = "aqua",
-		},
-	},
-	diagnostic_info = {
-		provider = "diagnostic_info",
-	},
-	lsp_client_names = {
-		provider = "lsp_client_names",
-		hl = {
-			fg = "purple",
-			bg = "darkblue",
-			style = "bold",
-		},
-		left_sep = "left_filled",
-		right_sep = "block",
-	},
-	file_type = {
-		provider = {
-			name = "file_type",
-			opts = {
-				filetype_icon = true,
-				case = "titlecase",
-			},
-		},
-		hl = {
-			fg = "red",
-			bg = "darkblue",
-			style = "bold",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	file_encoding = {
-		provider = "file_encoding",
-		hl = {
-			fg = "orange",
-			bg = "darkblue",
-			style = "italic",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	position = {
-		provider = "position",
-		hl = {
-			fg = "green",
-			bg = "darkblue",
-			style = "bold",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	line_percentage = {
-		provider = "line_percentage",
-		hl = {
-			fg = "aqua",
-			bg = "darkblue",
-			style = "bold",
-		},
-		left_sep = "block",
-		right_sep = "block",
-	},
-	scroll_bar = {
-		provider = "scroll_bar",
-		hl = {
-			fg = "yellow",
-			style = "bold",
-		},
-	},
+    vim_mode = {
+        provider = {
+            name = "vi_mode",
+            opts = {
+                show_mode_name = true,
+                -- padding = "center", -- Uncomment for extra padding.
+            },
+        },
+        hl = function()
+            return {
+                fg = require("feline.providers.vi_mode").get_mode_color(),
+                bg = "darkblue",
+                style = "bold",
+                name = "NeovimModeHLColor",
+            }
+        end,
+        left_sep = "block",
+        right_sep = "block",
+    },
+    gitBranch = {
+        provider = "git_branch",
+        hl = {
+            fg = "peanut",
+            bg = "darkblue",
+            style = "bold",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    gitDiffAdded = {
+        provider = "git_diff_added",
+        hl = {
+            fg = "green",
+            bg = "darkblue",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    gitDiffRemoved = {
+        provider = "git_diff_removed",
+        hl = {
+            fg = "red",
+            bg = "darkblue",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    gitDiffChanged = {
+        provider = "git_diff_changed",
+        hl = {
+            fg = "fg",
+            bg = "darkblue",
+        },
+        left_sep = "block",
+        right_sep = "right_filled",
+    },
+    separator = {
+        provider = "",
+    },
+    fileinfo = {
+        provider = {
+            name = "file_info",
+            opts = {
+                type = "relative-short",
+            },
+        },
+        hl = {
+            style = "bold",
+        },
+        left_sep = " ",
+        right_sep = " ",
+    },
+    diagnostic_errors = {
+        provider = "diagnostic_errors",
+        hl = {
+            fg = "red",
+        },
+    },
+    diagnostic_warnings = {
+        provider = "diagnostic_warnings",
+        hl = {
+            fg = "yellow",
+        },
+    },
+    diagnostic_hints = {
+        provider = "diagnostic_hints",
+        hl = {
+            fg = "aqua",
+        },
+    },
+    diagnostic_info = {
+        provider = "diagnostic_info",
+    },
+    lsp_client_names = {
+        provider = "lsp_client_names",
+        hl = {
+            fg = "purple",
+            bg = "darkblue",
+            style = "bold",
+        },
+        left_sep = "left_filled",
+        right_sep = "block",
+    },
+    file_type = {
+        provider = {
+            name = "file_type",
+            opts = {
+                filetype_icon = true,
+                case = "titlecase",
+            },
+        },
+        hl = {
+            fg = "red",
+            bg = "darkblue",
+            style = "bold",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    file_encoding = {
+        provider = "file_encoding",
+        hl = {
+            fg = "orange",
+            bg = "darkblue",
+            style = "italic",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    position = {
+        provider = "position",
+        hl = {
+            fg = "green",
+            bg = "darkblue",
+            style = "bold",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    line_percentage = {
+        provider = "line_percentage",
+        hl = {
+            fg = "aqua",
+            bg = "darkblue",
+            style = "bold",
+        },
+        left_sep = "block",
+        right_sep = "block",
+    },
+    scroll_bar = {
+        provider = "scroll_bar",
+        hl = {
+            fg = "yellow",
+            style = "bold",
+        },
+    },
 }
 
 local left = {
-	c.vim_mode,
-	c.gitBranch,
-	c.gitDiffAdded,
-  c.gitDiffRemoved,
-	c.gitDiffChanged,
-	c.separator,
+    c.vim_mode,
+    c.gitBranch,
+    c.gitDiffAdded,
+    c.gitDiffRemoved,
+    c.gitDiffChanged,
+    c.separator,
 }
 
 local middle = {
-	c.fileinfo,
-	c.diagnostic_errors,
-	c.diagnostic_warnings,
-	c.diagnostic_info,
-	c.diagnostic_hints,
+    c.fileinfo,
+    c.diagnostic_errors,
+    c.diagnostic_warnings,
+    c.diagnostic_info,
+    c.diagnostic_hints,
 }
 
 local right = {
-	c.lsp_client_names,
-	c.file_type,
-	c.file_encoding,
-	c.position,
-	c.line_percentage,
-	c.scroll_bar,
+    c.lsp_client_names,
+    c.file_type,
+    c.file_encoding,
+    c.position,
+    c.line_percentage,
+    c.scroll_bar,
 }
 
 local components = {
-	active = {
-		left,
-		middle,
-		right,
-	},
+    active = {
+        left,
+        middle,
+        right,
+    },
 }
 
 require('feline').setup({
-	components = components,
-	theme = one_monokai,
-	vi_mode_colors = vi_mode_colors,
+    components = components,
+    theme = one_monokai,
+    vi_mode_colors = vi_mode_colors,
 })
 
 -- AUTOPAIRS
@@ -465,3 +499,13 @@ require('feline').setup({
 require('nvim-autopairs').setup({
     disable_filetype = { "TelescopePrompt", "vim" },
 })
+
+-- *.md to pdf
+--
+require('md-pdf').setup({
+  --- Set margins around document
+  margins = "1.5cm",
+  --- tango, pygments are quite nice for white on white
+  highlight = "tango",
+})
+
